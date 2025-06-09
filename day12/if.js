f540165e78e3a3a7f916b95d275cd21d34ba3174
Sravanthi908 <mushinisravanthi09@gmail.com>
2025-06-09 19:55:41 +0300

    my first commit

---
+++ day12/if.js
@@ -0,0 +1,9 @@
+let x=90,y=20;
+
+if (x > y) {
+  console.log("y is smallest ");
+} else if (x < y) {
+  console.log("x is a smallest ");
+} else {
+  console.log("Keep Trying");
+}
---
+++ day12/try.js
@@ -0,0 +1,6 @@
+let x=90,y=30;
+console.log(x+y)
+console.log(x-y)
+console.log(x/y)
+console.log(x%y)
+console.log(x*y)
