67732b84775c05d3ea1728d9c3301d3b98308fb3
Sravanthi908 <mushinisravanthi09@gmail.com>
2025-06-18 06:12:34 +0300

    TODO App

---
+++ day19/src/componet/Counter.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+
+const Counter = () => {
+
+    const [balls , setBalls] = useState(0);
+    const [overs , setOvers] = useState(0);
+
+
+    const handleBalls = () =>{
+
+        if(balls + 1 == 6)
+        {
+            setOvers(overs + 1);
+            setBalls(0);
+        }
+        else
+            setBalls(balls + 1);
+    }
+
+    
+
+    useEffect(()=>{
+        console.log("side effect :" , overs , balls)
+    } , [balls , overs])
+
+  return (
+    <div>
+        <h1>Overs : {overs}.{balls}</h1>
+        <button onClick={handleBalls} >add ball</button>
+    </div>
+  )
+}
+
+export default Counter
